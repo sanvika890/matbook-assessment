49b3d41fa49972059162d37f2b03c19864a22cd5
Sanvika Kumar <sanvika890@gmail.com>
2025-03-28 20:01:02 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://matbook-assessment.vercel.app'],
+  origin: ['http://localhost:5173', 'https://matbook-assessment.vercel.app','https://matbook-assessment-yg63.vercel.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
